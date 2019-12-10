2f55017d3971fa37c9d857273984d1aa494d1052
xuh <254490061@qq.com>
2019-12-10 23:30:48 +0800

    删除测试类.移动serviceImpl

--- src/main/java/com/search/engine/service/impl/EngineServiceImpl.java
+++ src/main/java/com/search/engine/service/impl/EngineServiceImpl.java
@@ -1,5 +1,6 @@
-package com.search.engine.service;
+package com.search.engine.service.impl;
 
+import com.search.engine.service.EngineService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
